14c21ae898e8a4a8fdf96d766b43d95ceaa49a3b
vijaydurai <vijaydurai>
2026-05-30 18:45:29 +0530

    chore: pin provider versions for aws and tls

--- env/provider.tf
+++ env/provider.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.47.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.3.0"
+    }
+  }
+}
+
 # Configure the AWS Provider
 
 provider "aws" {
